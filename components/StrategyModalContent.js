75b3e0f7e74f83e4e4110d4233897b7133e15e70
Ankush Chauhan <ankushch2712@gmail.com>
2021-01-05 13:43:45 +0530

    Strategy selection based on module

--- components/StrategyModalContent.js
+++ components/StrategyModalContent.js
@@ -1,8 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { View, Text } from 'react-native';
 import { colors } from '../constants';
 import { FilterPill } from './button';
 import { FilterContext } from '../contexts/FilterContext';
+import { AuthContext } from '../contexts/AuthContext';
+import { Snackbar } from 'react-native-paper';
 
 const intraday = [
   { name: '5 Min', key: 'WLF' },
@@ -17,7 +19,10 @@ const positional = [
 ];
 
 const StrategyModalContent = () => {
+  const { loginState } = useContext(AuthContext);
+  const activeModule = loginState.userData.module;
   const { filterState, filterDispatch } = useContext(FilterContext);
+  const [visible, setVisible] = useState(false);
 
   const setStrategyFilter = (strategy) => {
     if (strategy.key != '') {
@@ -57,26 +62,27 @@ const StrategyModalContent = () => {
           borderBottomWidth: 1,
           borderBottomColor: colors.secondaryLightColor,
         }}>
-        {filterState && intraday.map((item, i) => (
-          <View key={i} style={{ marginRight: 10, marginBottom: 10 }}>
-            <FilterPill
-              onPress={() => {
-                setStrategyFilter(item);
-              }}
-              text={item.name}
-              bgColor={
-                filterState.strategy.name == item.name
-                  ? colors.accentColor
-                  : colors.secondaryLightColor
-              }
-              textColor={
-                filterState.strategy.name == item.name
-                  ? colors.lightColor
-                  : colors.secondaryColor
-              }
-            />
-          </View>
-        ))}
+        {filterState &&
+          intraday.map((item, i) => (
+            <View key={i} style={{ marginRight: 10, marginBottom: 10 }}>
+              <FilterPill
+                onPress={() => {
+                  setStrategyFilter(item);
+                }}
+                text={item.name}
+                bgColor={
+                  filterState.strategy.name == item.name
+                    ? colors.accentColor
+                    : colors.secondaryLightColor
+                }
+                textColor={
+                  filterState.strategy.name == item.name
+                    ? colors.lightColor
+                    : colors.secondaryColor
+                }
+              />
+            </View>
+          ))}
       </View>
       <Text
         style={{
@@ -99,27 +105,37 @@ const StrategyModalContent = () => {
           borderBottomWidth: 1,
           borderBottomColor: colors.secondaryLightColor,
         }}>
-        {filterState && positional.map((item, i) => (
-          <View key={i} style={{ marginRight: 10, marginBottom: 10 }}>
-            <FilterPill
-              onPress={() => {
-                setStrategyFilter(item);
-              }}
-              text={item.name}
-              bgColor={
-                filterState.strategy.name == item.name
-                  ? colors.accentColor
-                  : colors.secondaryLightColor
-              }
-              textColor={
-                filterState.strategy.name == item.name
-                  ? colors.lightColor
-                  : colors.secondaryColor
-              }
-            />
-          </View>
-        ))}
+        {filterState &&
+          positional.map((item, i) => (
+            <View key={i} style={{ marginRight: 10, marginBottom: 10 }}>
+              <FilterPill
+                onPress={() => {
+                  if (activeModule == '3') {
+                    setStrategyFilter(item);
+                  } else {
+                    console.log('Only for positional');
+                    setVisible(true);
+                    return;
+                  }
+                }}
+                text={item.name}
+                bgColor={
+                  filterState.strategy.name == item.name
+                    ? colors.accentColor
+                    : colors.secondaryLightColor
+                }
+                textColor={
+                  filterState.strategy.name == item.name
+                    ? colors.lightColor
+                    : colors.secondaryColor
+                }
+              />
+            </View>
+          ))}
       </View>
+      <Snackbar visible={visible} duration={3000} onDismiss={() => setVisible(false)}>
+        Feature available only on Positional plan
+      </Snackbar>
     </View>
   );
 };
